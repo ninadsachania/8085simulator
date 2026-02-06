ca1d9157c8efdf7871b9e00b82ff07ecf1a44df3
Ninad Sachania <ninad.sachania@gmail.com>
2026-02-06 19:27:38 +0530

    Use Maven as the build tool

---
+++ src/test/java/io/github/intel8085simulator/Intel8085SimulatorTest.java
@@ -0,0 +1,15 @@
+package io.github.intel8085simulator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Intel8085SimulatorTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
